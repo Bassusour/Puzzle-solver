edd5f95fad2c72ac19e29e06396fa1341629b7eb
Victor Andersen <s194577@student.dtu.dk>
2021-06-10 12:48:14 +0200

    Centering after auto solve + some cleanup

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,30 +1,6 @@
 package application;
-import java.awt.geom.Point2D;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Parent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Cursor;
-import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.LineTo;
-import javafx.scene.shape.MoveTo;
-import javafx.scene.shape.Path;
-import javafx.scene.shape.PathElement;
-import javafx.scene.shape.Polygon;
-import javafx.scene.shape.Shape;
-import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -38,8 +14,7 @@ public class Main extends Application {
 		//Model and View only communicate through this class
 //		Group groups = model.getGroups();
 		view.setView();
-		
-		
+
 	}
 
 	public static void main(String[] args) {
